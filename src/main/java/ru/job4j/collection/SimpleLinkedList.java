44306f7c6310ab5de5b1b19d2d61c04f880e6614
KEWNRU <n0kky21rus@gmail.com>
2023-07-11 10:35:40 +0300

    1. Динамический список на массиве. [#158]

---
+++ src/main/java/ru/job4j/collection/SimpleLinkedList.java
@@ -0,0 +1,54 @@
+package ru.job4j.collection;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class SimpleLinkedList<E> implements LinkedList<E> {
+
+    private int size = 0;
+    private int modCount = 0;
+    private Node<E> head;
+
+    @Override
+    public void add(E value) {
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            int point = 0;
+            int exprctionModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                if (exprctionModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return point < size;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return null;
+            }
+        };
+    }
+    private static class Node<E> {
+        private E item;
+        private Node<E> next;
+
+        Node(E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+        }
+    }
+}
