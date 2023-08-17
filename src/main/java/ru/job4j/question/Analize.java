7055e4fd9e00c2825110b70adc7bd501e509ec2c
KEWNRU <n0kky21rus@gmail.com>
2023-08-17 12:12:47 +0300

    3. Статистика по коллекции. [#45889]

--- src/main/java/ru/job4j/question/Analize.java
+++ src/main/java/ru/job4j/question/Analize.java
@@ -7,38 +7,27 @@ import java.util.Set;
 public class Analize {
 
     public static Info diff(Set<User> previous, Set<User> current) {
+        Map<Integer, String> map = new HashMap<>();
         Info result = new Info(0, 0, 0);
         int added = 0;
         int changed = 0;
-        int deleted = 0;
-        Map<Integer, String> prev = new HashMap<>();
-        Map<Integer, String> curr = new HashMap<>();
         for (User user : previous) {
-            prev.put(user.getId(), user.getName());
+            map.put(user.getId(), user.getName());
         }
         for (User user : current) {
-            curr.put(user.getId(), user.getName());
-        }
-        for (Map.Entry<Integer, String> el : curr.entrySet()) {
-            if (!prev.containsKey(el.getKey())) {
+            if (!map.containsKey(user.getId())) {
                 added++;
                 result.setAdded(added++);
             }
-        }
-        for (Map.Entry<Integer, String> el1 : prev.entrySet()) {
-            if (!curr.containsKey(el1.getKey())) {
-                deleted++;
-                result.setDeleted(deleted);
+            if (map.containsKey(user.getId()) && !map.containsValue(user.getName())) {
+                changed++;
+                result.setChanged(changed);
             }
-            for (Map.Entry<Integer, String> el2 : curr.entrySet()) {
-                if (el1.getKey().equals(el2.getKey())
-                        && !el1.getValue().equals(el2.getValue())) {
-                    changed++;
-                    result.setChanged(changed);
-                }
-            }
+            map.remove(user.getId());
 
         }
+        result.setDeleted(map.size());
         return result;
     }
+
 }
