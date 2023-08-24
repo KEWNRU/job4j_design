c28a7f0fe456973ac2473e379a06ef6e8b384413
KEWNRU <n0kky21rus@gmail.com>
2023-08-24 13:08:05 +0300

    2.0. FileOutputStream [#252488 #439314]

--- src/main/java/ru/job4j/io/Calculator.java
+++ src/main/java/ru/job4j/io/Calculator.java
@@ -1,6 +1,7 @@
 package ru.job4j.io;
 
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class Calculator {
     public static void main(String[] args) {
@@ -18,7 +19,7 @@ public class Calculator {
             out.write(System.lineSeparator().getBytes());
             out.write((String.valueOf(fourTimeTwo)).getBytes());
             out.write(System.lineSeparator().getBytes());
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
