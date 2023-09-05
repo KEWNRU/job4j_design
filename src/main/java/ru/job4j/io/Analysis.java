6499e03b7716a4a6b66bbf79624d9dec48fc0499
KEWNRU <n0kky21rus@gmail.com>
2023-09-05 10:57:04 +0300

    7. Анализ доступности сервера [#859]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -6,19 +6,16 @@ public class Analysis {
     public void unavailable(String source, String target) {
         try (BufferedReader read = new BufferedReader(new FileReader(source));
              PrintWriter writer = new PrintWriter(new FileOutputStream(target))) {
+            StringBuilder builder = new StringBuilder();
             boolean server = false;
             while (read.ready()) {
                 String line = read.readLine();
-                String[] status = line.split(" ");
-                if (server && (!line.startsWith("400") && !line.startsWith("500"))) {
-                    server = false;
-                    writer.write(String.format("%s%s%s", status[1], "; ", System.lineSeparator()));
-                }
-                if (!server && (line.startsWith("400") || line.startsWith("500"))) {
-                    server = true;
-                    writer.write(String.format("%s%s", status[1], "; "));
+                if (server == (line.startsWith("200") || line.startsWith("300"))) {
+                    server = !server;
+                   builder.append(line.substring(4)).append(server ? ";" : System.lineSeparator());
                 }
             }
+            writer.write(builder.toString());
         } catch (IOException e) {
             e.printStackTrace();
         }
