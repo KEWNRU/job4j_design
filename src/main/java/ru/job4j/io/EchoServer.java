bb4f0ee6c274fdd9ecc138e4ca12bf68ae174035
KEWNRU <n0kky21rus@gmail.com>
2024-01-09 22:16:24 +0300

    4. Slf4j - вывод exception. [#268853]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,35 +1,44 @@
 package ru.job4j.io;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-    public static void main(String[] args) throws IOException {
-        try (ServerSocket server = new ServerSocket(9000)) {
-            while (!server.isClosed()) {
-                Socket socket = server.accept();
-                try (OutputStream output = socket.getOutputStream();
-                     BufferedReader input = new BufferedReader(
-                             new InputStreamReader(socket.getInputStream()))) {
-                    output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    String str = input.readLine();
-                    if (str.contains("msg=Hello")) {
-                        output.write("Hello, dear friend.".getBytes());
+    private static final Logger LOG = LoggerFactory.getLogger(EchoServer.class.getName());
+
+    public static void main(String[] args) {
+        try {
+            try (ServerSocket server = new ServerSocket(9000)) {
+                while (!server.isClosed()) {
+                    Socket socket = server.accept();
+                    try (OutputStream output = socket.getOutputStream();
+                         BufferedReader input = new BufferedReader(
+                                 new InputStreamReader(socket.getInputStream()))) {
+                        output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        String str = input.readLine();
+                        if (str.contains("msg=Hello")) {
+                            output.write("Hello, dear friend.".getBytes());
+                        }
+                        if (str.contains("msg=What")) {
+                            output.write("What".getBytes());
+                        }
+                        if (str.contains("msg=Exit")) {
+                            output.write("Server is close".getBytes());
+                            server.close();
+                        }
+                        for (String string = input.readLine(); string != null && !string.isEmpty(); string = input.readLine()) {
+                            System.out.println(string);
+                        }
+                        output.flush();
                     }
-                    if (str.contains("msg=What")) {
-                        output.write("What".getBytes());
-                    }
-                    if (str.contains("msg=Exit")) {
-                        output.write("Server is close".getBytes());
-                        server.close();
-                    }
-                    for (String string = input.readLine(); string != null && !string.isEmpty(); string = input.readLine()) {
-                        System.out.println(string);
-                    }
-                    output.flush();
                 }
             }
+        } catch (IOException e) {
+            LOG.error("Exception in log example", e);
         }
     }
 }
