064e779962d18a08f47dcf9d0e79cc4f5a5b6bca
KEWNRU <n0kky21rus@gmail.com>
2024-01-09 19:24:47 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,16 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Petr Arsentev";
+        int age = 33;
+        byte list = 11;
+        float list1 = 33.1f;
+        double list2 = 22.5d;
+        boolean list3 = false;
+        char a = 'a';
+        long list4 = -2L;
+        short list5 = 21;
+        LOG.debug("User info name : {}, age : {}, list : {}, list1 {}, list2 {}, list3 {}, list4 {}, list5 {}, char {}"
+                , name, age, list, list1, list2, list3, list4, list5, a);
     }
 }
