f07c6bfa3dffd9f00a21afea57d7f87c83da39da
KEWNRU <n0kky21rus@gmail.com>
2024-01-10 14:53:01 +0300

    4. JAXB. Преобразование XML в POJO. [#315063]

---
+++ src/main/java/ru/job4j/serialization/xml/Main.java
@@ -0,0 +1,36 @@
+package ru.job4j.serialization.xml;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Gamer gamer = new Gamer(true, 26, new Rank("level5"),
+                "CS2, Faceit, Play");
+
+        /* Получаем контекст для доступа к АПИ */
+        JAXBContext context = JAXBContext.newInstance(Gamer.class);
+        /* Создаем сериализатор */
+        Marshaller marshaller = context.createMarshaller();
+        /* Указываем, что нам нужно форматирование */
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        String xml = "";
+        try (StringWriter writer = new StringWriter()) {
+            /* Сериализуем */
+            marshaller.marshal(gamer, writer);
+            xml = writer.getBuffer().toString();
+            System.out.println(xml);
+        }
+        /* Для десериализации нам нужно создать десериализатор */
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        try (StringReader reader = new StringReader(xml)) {
+            /* десериализуем */
+            Gamer result = (Gamer) unmarshaller.unmarshal(reader);
+            System.out.println(result);
+        }
+
+    }
+}
