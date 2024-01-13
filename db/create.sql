b7b6ac993586d0185c4f5a28cfa512e44b759ca2
KEWNRU <n0kky21rus@gmail.com>
2024-01-13 22:03:11 +0300

    2. Схема прав пользователи и роли. [#1093 #473114]

--- db/create.sql
+++ db/create.sql
@@ -1,12 +1,12 @@
 create table users (
 id serial primary key,
-users_name text
+users_name text,
+roles_id int refernces roles(id)
 );
 
 create table roles (
 id serial primary key,
 roles_name text,
-users_id int references users(id)
 );
 
 create table rules (
@@ -23,7 +23,9 @@ rules_id int references rules(id)
 create table items (
 id serial primary key,
 items_name text,
-users_id int references users(id)
+users_id int references users(id),
+categories_id int references categories(id),
+states_id int references states(id)
 );
 
 create table comments (
@@ -41,11 +43,9 @@ items_id int references items(id)
 create table states (
 id serial primary key,
 states_name text,
-items_id int references items(id)
 );
 
 create table categories (
 id serial primary key,
 categories_name text,
-items_id int references items(id)
 );
